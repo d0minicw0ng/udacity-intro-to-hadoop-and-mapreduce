b6531cd48cadbfacdc51cd30802d4b208aada05c
Dominic Wong <dominic.wong.617@gmail.com>
2015-03-04 03:28:24 +0800

    Lesson 4 Exercise 3

---
+++ lesson4/exercise3/reducer.py
@@ -0,0 +1,21 @@
+import sys
+import re
+import collections
+
+def reducer():
+    inverted_index = collections.defaultdict(list)
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        if len(data) != 2:
+            continue
+
+        word = data[1].lower()
+        inverted_index[word].append(data[0])
+
+    for word in inverted_index:
+        print "{0}\t{1}\t{2}".format(word, len(inverted_index[word]), inverted_index[word])
+
+def main():
+    reducer()
+
+main()
