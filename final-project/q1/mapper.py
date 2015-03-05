9ac0221272fc51e5f365e734bd6a87fa74acba4e
Dominic Wong <dominic.wong.617@gmail.com>
2015-03-05 09:56:43 +0800

    Final Project Q1

---
+++ final-project/q1/mapper.py
@@ -0,0 +1,18 @@
+import csv
+import sys
+
+def mapper():
+    reader = csv.reader(sys.stdin, delimiter='\t')
+    for line in reader:
+        author_id = line[3]
+        added_at = line[8]
+
+        if author_id == "author_id":
+            continue
+
+        print "{0}\t{1}".format(author_id, added_at)
+
+def main():
+    mapper()
+
+main()
